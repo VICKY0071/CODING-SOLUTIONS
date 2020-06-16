03b2058ee21301e3345e6a77b507129f6571ee98
Vikas Thapliyal <40348389+VICKY0071@users.noreply.github.com>
2020-06-16 22:13:43 +0530

    Add files via upload
    added the solution for challenge today

---
+++ containsCloseNumber.cpp
@@ -0,0 +1,15 @@
+bool containsCloseNums(std::vector<int> nums, int k) {
+    unordered_map<int, vector<int>> a;
+    for(int i = 0;i<nums.size();i++){
+        if(a.find(nums[i]) != a.end()){
+            vector<int> values = a[nums[i]];
+            for(int index : values){
+                if(abs(index - i) <= k){
+                    return true;
+                }
+            }
+        }
+        a[nums[i]].push_back(i);
+    }
+    return false;
+}
